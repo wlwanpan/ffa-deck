454633774556932c382cb2cddb5a7776bdffee14
wlwanpan <wlwanpan@uwaterloo.ca>
2017-09-23 22:19:39 -0700

    migration channel

---
+++ app/channels/connector_channel.rb
@@ -0,0 +1,11 @@
+class ConnectorChannel < ApplicationCable::Channel  
+  
+  def subscribed
+    stream_from "connector-#{params[:tunnel]}"
+  end
+
+  def unsubscribed
+    stop_all_streams
+  end
+
+end
