b68b0c66b6589af1def300dec0bce42b4ce6d0e4
wlwanpan <wlwanpan@uwaterloo.ca>
2017-10-05 23:09:36 -0700

    cached id->channel_uuid

---
+++ app/controllers/concerns/multiplexer.rb
@@ -0,0 +1,23 @@
+module Multiplexer
+  @@cache ||= ActiveSupport::Cache::MemoryStore.new
+
+  def cache_channel(account_id, channel_id)
+    @@cache.write("account_#{account_id}", channel_id)
+  end
+
+  def fetch_channel(account_id)
+    @@cache.fetch("account_#{account_id}")
+  end
+
+  def broadcast_to_all(from, data)
+    ActionCable.server.broadcast "connector-global", from: from, data: data
+  end
+
+  def broadcast_to(id, data)
+    channel_uuid = fetch_channel(id)
+    unless channel_uuid.blank?
+      ActionCable.server.broadcast "connector-#{channel_uuid}", from: id, data: data
+    end
+  end
+
+end
